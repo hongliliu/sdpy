50f2ce8d1e90e0ce1ea5f6aa51a9dc9c117a3198
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-12-16 22:08:32 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='gbtpy',
+      version='0.0.0.dev',
+      description='GBT python tools',
+      author='Adam Ginsburg',
+      author_email='adam.g.ginsburg@gmail.com',
+      packages=['gbtpy'],
+      provides=['gbtpy'],
+      requires=['numpy'],
+      keywords=['Scientific/Engineering'],
+      )
