dab2c336cf46c41b5e5d9b836a995d66c0b94204
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-13 17:27:01 +0100

    stringy units

--- gbtpy/make_off_template.py
+++ gbtpy/make_off_template.py
@@ -28,7 +28,7 @@ def generate_1d_header_fromdisparray(arr, cdelt_tolerance=1e-8, reference=None,
         arr = arr.value
 
     # convert string to unit
-    if not hasattr(unit,'name'):
+    if not hasattr(unit,'to_string'):
         unit = u.Unit(unit)
     
     # determine CDELT
@@ -49,7 +49,7 @@ def generate_1d_header_fromdisparray(arr, cdelt_tolerance=1e-8, reference=None,
     header['CRVAL1'] = crval
     header['CDELT1'] = cdelt
     header['CRPIX1'] = crpix
-    header['CUNIT1'] = unit.name
+    header['CUNIT1'] = unit.to_string()
     header['CTYPE1'] = type_to_ctype(unit.physical_type)
 
     return header
