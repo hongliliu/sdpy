3e2c2e470204161439e48a66c7e365c109e020dd
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-07-30 11:40:51 +0200

    verbosity changes

--- sdpy/makecube.py
+++ sdpy/makecube.py
@@ -475,6 +475,9 @@ def add_data_to_cube(cubefilename, data=None, filename=None, fileheader=None,
                                  coord_iterator(data,coordsys_out=coordsys),
                                  velo_iterator(data,linefreq=linefreq)):
 
+        if log.level <= 10:
+            t1 = time.time()
+
         if not hasattr(velo,'unit'):
             velo = velo * default_unit
 
@@ -625,6 +628,10 @@ def add_data_to_cube(cubefilename, data=None, filename=None, fileheader=None,
             if debug_breakpoint:
                 import ipdb
                 ipdb.set_trace()
+
+        if log.level <= 10:
+            dt = time.time() - t1
+            log.debug("Completed x,y={x:4.0f},{y:4.0f} ({x:6.2f},{y:6.2f}) in {dt:<5.1f}s".format(x=x,y=y,dt=dt))
 
     if excludefitrange is not None:
         # this block redefining "include" is used for diagnostics (optional)
